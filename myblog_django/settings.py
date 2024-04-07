02c00208329671a72312e3c8ba9f2eeffbcae62e
Irek <i_kar@bk.ru>
2024-04-07 22:03:23 +0500

    users app. Меню Навигации и Ссылки для Users

--- myblog_django/settings.py
+++ myblog_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
--- myblog_django/urls.py
+++ myblog_django/urls.py
@@ -14,16 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-
+import blog
+import users.views
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.MainView.as_view()),
-    path("post/", views.PostDetail.as_view()),
-    path("contact/", views.ContactForm.as_view()),
+    path("", blog.views.MainView.as_view(), name='index'),
+    path("login/", users.views.login, name='login'),
+    path("logout/", users.views.logout, name='logout'),
+    path("registration/", users.views.registration, name='registration'),
+    path("profile/", users.views.profile, name='profile'),
+    path("post/", views.PostDetail.as_view(), name='post'),
+    path("contact/", views.ContactForm.as_view(), name='contact'),
+    path('user/', include('users.urls', namespace='user')),
     #path("", blog.views.index),
 ]
+
+#if settings.DEBUG:
+#    urlpatterns += [
+#        path("__debug__/", include("debug_toolbar.urls")),
+#    ]
+
+# Для возможности закачки файлов
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
